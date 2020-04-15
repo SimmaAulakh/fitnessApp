7c392a143b7ac91c77fabc277cf0b54a5710c4a2
Simranjeet <simranjeetaulakh@Simranjeets-MacBook-Pro.local>
2020-04-15 23:28:38 +0530

    screens development

---
+++ Grrind Fitness/ViewControllers/FacebookRegisterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FacebookRegisterViewController.swift
+//  Grrind Fitness
+//
+//  Created by Simranjeet Aulakh on 15/04/20.
+//  Copyright © 2020 aulakh. All rights reserved.
+//
+
+import UIKit
+
+class FacebookRegisterViewController: UIViewController {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        ImageView.layer.borderWidth = 2
+        ImageView.layer.borderColor = #colorLiteral(red: 0.2117647059, green: 0.8274509804, blue: 0.8823529412, alpha: 1).cgColor
+    }
+    
+
+   
+    }
+    
+
+
